96d03acba40f822caf75a67e0d03d4a3f5fb43fe
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2022-05-26 23:19:50 +0430

    Making the testimonial section.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,33 @@ $(document).ready(function() {
         $(".work").removeClass("hovered--work");
     });
 });
+
+let slideIndex = 0;
+showSlide();
+
+function showSlide() {
+    let i;
+    let slides = document.getElementsByClassName('mySlide');
+    let dots1 = document.getElementsByClassName('testimonial1__dots');
+    let dots2 = document.getElementsByClassName('testimonial2__dots');
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = "0";
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length)
+    slideIndex = 1;
+    
+    for (i = 0; i < dots1.length; i++) {
+        dots1[i].className = dots1[i].className.replace(' active', '');
+    }
+    for (i = 0; i < dots2.length; i++) {
+        dots2[i].className = dots2[i].className.replace(' active', '');
+    }
+    
+    slides[slideIndex - 1].style.opacity = '1';
+    dots1[slideIndex - 1].className += ' active';
+    dots2[slideIndex - 1].className += ' active';
+    setTimeout(showSlide, 7000);
+}
